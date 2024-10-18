4ceb21ef72174824884722d4b5917d471d1feefd
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-10-18 21:56:01 +0700

    membuat text login

--- app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
@@ -51,6 +51,8 @@ fun BasicColumn(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Top,
         modifier = Modifier.fillMaxSize())
     {
+        Text("Login",
 
+        )
     }
 }
