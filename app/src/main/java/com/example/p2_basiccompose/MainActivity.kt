615eff55be1e4ea203da3a13e569701457ee7f1c
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-10-19 00:22:01 +0700

    mengatur bentuk sudut agar membulat

--- app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2_basiccompose/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
@@ -80,6 +81,9 @@ fun BasicColumn(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .padding(16.dp)
                 .size(250.dp, 420.dp)
+                .clip(RoundedCornerShape(150.dp)))
 
     }
 }
+
+
